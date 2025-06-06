ccd98306e2a9878821def3014d57d47278580d58
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-06-06 13:03:51 +0530

    Update hello_world.v
    Hello world program in verilog HDL.

--- Basics/hello_world.v
+++ Basics/hello_world.v
@@ -1,7 +1,7 @@
 // Hello World - Example
 module hello_world();
 
-  // Displaysthe message in the console on a new line with a TAB before
+  // Displays the message in the console on a new line with a TAB before
   initial begin
     $display(("\n\t Hello world! \n");
 	
