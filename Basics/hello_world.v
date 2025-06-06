4d4905e53edbe966bec5229477d58526051a743e
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-06-06 12:18:47 +0530

    Create hello_world.v
    Hello world program in Verilog HDL.

---
+++ Basics/hello_world.v
@@ -0,0 +1,10 @@
+// Hello World - Example
+module hello_world();
+
+  // Displaysthe message in the console on a new line with a TAB before
+  initial begin
+    $display(("\n\t Hello world! \n");
+	
+  end
+
+endmodule
