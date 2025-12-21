3662603fa7cab483b97522a77750b135208b58cc
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-21 09:07:57 +0530

    Update shift_left_right_load_reg.v

--- Verilog_Sequential_Design/shift_left_right_load_reg.v
+++ Verilog_Sequential_Design/shift_left_right_load_reg.v
@@ -16,9 +16,9 @@ module shift_left_right_load_reg(
 			q <= I;
 		else begin 
 			if(shift_left_right == 1'b0) begin	// Shift register
-				q <= {I[6:0],1'b0};				// Left shift
+				q <= {q[6:0],1'b0};				// Left shift
 			end else begin
-				q <= {1'b0,I[7:1]};				// Right shift
+				q <= {1'b0,q[7:1]};				// Right shift
 			end
 		end
 	end
@@ -29,4 +29,5 @@ endmodule
 	
 		
 	
+
 		 
